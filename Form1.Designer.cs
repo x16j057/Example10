84ac39bd0adfbf9c647cda96688f4aed5628f260
WIN_NT\x16j057 <x16j057@chiba-fjb.ac.jp>
2017-05-17 12:28:33 +0900

    畏怖分

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Example10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxAge = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnJudge = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "年齢は";
+            // 
+            // tbxAge
+            // 
+            this.tbxAge.Location = new System.Drawing.Point(101, 31);
+            this.tbxAge.Name = "tbxAge";
+            this.tbxAge.Size = new System.Drawing.Size(57, 19);
+            this.tbxAge.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "歳";
+            // 
+            // btnJudge
+            // 
+            this.btnJudge.Location = new System.Drawing.Point(231, 31);
+            this.btnJudge.Name = "btnJudge";
+            this.btnJudge.Size = new System.Drawing.Size(26, 78);
+            this.btnJudge.TabIndex = 3;
+            this.btnJudge.Text = "判定";
+            this.btnJudge.UseVisualStyleBackColor = true;
+            this.btnJudge.Click += new System.EventHandler(this.btnJudge_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(164, 93);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 12);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnJudge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxAge);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxAge;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnJudge;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJudge_Click(object sender, EventArgs e)
+        {
+            int age = int.Parse(tbxAge.Text);
+            if (age >= 20)
+                lblResult.Text = "成人です";
+            else if (age < 20)
+                lblResult.Text = "未成年です";
+            if (age == 20)
+                lblResult.Text = "還暦です";
+
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
